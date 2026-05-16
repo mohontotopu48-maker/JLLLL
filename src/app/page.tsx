0f188f505d6219a201ab95d90739947b0028fd09
Z User <z@container>
2026-05-16 06:34:42 +0000

    99f9ce8e-45f3-498e-8918-b962076e03cd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ function Navigation() {
           <img
             src="/images/jl-logo.svg"
             alt="JL Custom Upholstery Logo"
-            className="h-11 w-auto"
+            className="h-10 w-auto"
           />
           <span className="text-champagne text-2xl font-black tracking-tight">
             JL
@@ -175,7 +175,7 @@ function Navigation() {
                   <img
                     src="/images/jl-logo.svg"
                     alt="JL Logo"
-                    className="h-9 w-auto"
+                    className="h-8 w-auto"
                   />
                   <span className="text-champagne text-lg font-black">
                     JL <span className="text-white font-light tracking-[0.2em] text-sm">CUSTOM UPHOLSTERY</span>
